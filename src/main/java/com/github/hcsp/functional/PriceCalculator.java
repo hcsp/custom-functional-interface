780e302fa1482828074704d4bca958726ca8671b
Suyu <zhuangsuyu1993@163.com>
2019-09-10 17:37:01 +0800

    函数式接口 (#3)
    函数式接口 (#3)

--- src/main/java/com/github/hcsp/functional/PriceCalculator.java
+++ src/main/java/com/github/hcsp/functional/PriceCalculator.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.functional;
 
+import java.util.function.BiFunction;
+
 public class PriceCalculator {
     public static void main(String[] args) {
         int originalPrice = 100;
@@ -14,6 +16,7 @@ public class PriceCalculator {
                 originalPrice,
                 vipUser);
     }
+
     // 还记得策略模式么？有了函数式接口之后，策略模式的实现就更加简单了
     // 使用函数式接口重构这个方法，将原先的三种策略作为参数传入
     //
@@ -23,23 +26,7 @@ public class PriceCalculator {
     // 或是使用JDK自带的函数式接口BiFunction
     //
     // static int calculatePrice(BiFunction<Integer,User,Integer> strategy, int price, User user)
-
-    public static int calculatePrice(String discountStrategy, int price, User user) {
-        switch (discountStrategy) {
-            case "NoDiscount":
-                return price;
-            case "Discount95":
-                return (int) (price * 0.95);
-            case "OnlyVip":
-                {
-                    if (user.isVip()) {
-                        return (int) (price * 0.95);
-                    } else {
-                        return price;
-                    }
-                }
-            default:
-                throw new IllegalStateException("Should not be here!");
-        }
+    public static int calculatePrice(BiFunction<Integer, User, Integer> strategy, int price, User user) {
+        return strategy.apply(price, user);
     }
 }
