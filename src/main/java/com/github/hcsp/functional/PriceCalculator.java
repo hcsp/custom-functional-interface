f81c2889273ea96be0bfc6df408e7aa269fca577
Yuyan <845058547@qq.com>
2019-09-17 15:29:02 +0800

    使用函数式编程实现策略模式 1.使用BIFunction接口实现 2.使用自定义的接口实现

--- src/main/java/com/github/hcsp/functional/PriceCalculator.java
+++ src/main/java/com/github/hcsp/functional/PriceCalculator.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.functional;
 
+import java.util.function.BiFunction;
+
 public class PriceCalculator {
     public static void main(String[] args) {
         int originalPrice = 100;
@@ -7,13 +9,14 @@ public class PriceCalculator {
         // 不打折
         calculatePrice((price, user) -> price, originalPrice, vipUser);
         // 全场95折
-        calculatePrice((price, user) -> (int) (price * 0.95), originalPrice, vipUser);
+        discountMoney((price, user) -> (int) (price * 0.95), originalPrice, vipUser);
         // 只有VIP打95折，其他人保持原价
         calculatePrice(
                 (price, user) -> user.isVip() ? (int) (price * 0.95) : price,
                 originalPrice,
                 vipUser);
     }
+
     // 还记得策略模式么？有了函数式接口之后，策略模式的实现就更加简单了
     // 使用函数式接口重构这个方法，将原先的三种策略作为参数传入
     //
@@ -22,24 +25,15 @@ public class PriceCalculator {
     //
     // 或是使用JDK自带的函数式接口BiFunction
     //
-    // static int calculatePrice(BiFunction<Integer,User,Integer> strategy, int price, User user)
+    static int calculatePrice(BiFunction<Integer, User, Integer> strategy, int price, User user) {
+        return strategy.apply(price, user);
+    }
 
-    public static int calculatePrice(String discountStrategy, int price, User user) {
-        switch (discountStrategy) {
-            case "NoDiscount":
-                return price;
-            case "Discount95":
-                return (int) (price * 0.95);
-            case "OnlyVip":
-                {
-                    if (user.isVip()) {
-                        return (int) (price * 0.95);
-                    } else {
-                        return price;
-                    }
-                }
-            default:
-                throw new IllegalStateException("Should not be here!");
-        }
+    public interface DiscountStrategy<T, U, R> {
+        R Discount(T t, U u);
+    }
+
+    private static int discountMoney(DiscountStrategy<Integer, User, Integer> discountStrategy, int price, User user) {
+        return discountStrategy.Discount(price, user);
     }
 }
