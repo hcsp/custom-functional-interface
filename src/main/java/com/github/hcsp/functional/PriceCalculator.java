66d3ffe217fe9548595d7f55d80547a8cd63e1a6
Procarihana <48359478+Procarihana@users.noreply.github.com>
2019-10-13 23:38:42 +0800

    分别用函数式接口BiFunction 和 自制接口重构 (#12)
    分别用函数式接口BiFunction 和 自制接口重构 (#12)

--- src/main/java/com/github/hcsp/functional/PriceCalculator.java
+++ src/main/java/com/github/hcsp/functional/PriceCalculator.java
@@ -1,5 +1,6 @@
 package com.github.hcsp.functional;
 
+
 public class PriceCalculator {
     public static void main(String[] args) {
         int originalPrice = 100;
@@ -14,6 +15,7 @@ public class PriceCalculator {
                 originalPrice,
                 vipUser);
     }
+
     // 还记得策略模式么？有了函数式接口之后，策略模式的实现就更加简单了
     // 使用函数式接口重构这个方法，将原先的三种策略作为参数传入
     //
@@ -23,23 +25,30 @@ public class PriceCalculator {
     // 或是使用JDK自带的函数式接口BiFunction
     //
     // static int calculatePrice(BiFunction<Integer,User,Integer> strategy, int price, User user)
-
-    public static int calculatePrice(String discountStrategy, int price, User user) {
-        switch (discountStrategy) {
-            case "NoDiscount":
-                return price;
-            case "Discount95":
-                return (int) (price * 0.95);
-            case "OnlyVip":
-                {
-                    if (user.isVip()) {
-                        return (int) (price * 0.95);
-                    } else {
-                        return price;
-                    }
-                }
-            default:
-                throw new IllegalStateException("Should not be here!");
-        }
+    interface DiscountStrategy {
+        int discount(int price, User user);
     }
+
+    static int calculatePrice(DiscountStrategy strategy, int price, User user) {
+        return strategy.discount(price, user);
+    }
+
+//    public static int calculatePrice(String discountStrategy, int price, User user) {
+//        switch (discountStrategy) {
+//            case "NoDiscount":
+//                return price;
+//            case "Discount95":
+//                return (int) (price * 0.95);
+//            case "OnlyVip":
+//                {
+//                    if (user.isVip()) {
+//                        return (int) (price * 0.95);
+//                    } else {
+//                        return price;
+//                    }
+//                }
+//            default:
+//                throw new IllegalStateException("Should not be here!");
+//        }
+//    }
 }
