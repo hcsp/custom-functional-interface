e77572875b7a86d7aef3d8ce57afc321b83dac3a
gofocus <go_focus@live.cn>
2020-04-27 16:13:45 +0800

    自定义函数接口 (#71)
    自定义函数接口 (#71)

--- src/main/java/com/github/hcsp/functional/PriceCalculator.java
+++ src/main/java/com/github/hcsp/functional/PriceCalculator.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.functional;
 
+import java.util.function.BiFunction;
+
 public class PriceCalculator {
     public static void main(String[] args) {
         int originalPrice = 100;
@@ -24,22 +26,7 @@ public class PriceCalculator {
     //
     // static int calculatePrice(BiFunction<Integer,User,Integer> strategy, int price, User user)
 
-    public static int calculatePrice(String discountStrategy, int price, User user) {
-        switch (discountStrategy) {
-            case "NoDiscount":
-                return price;
-            case "Discount95":
-                return (int) (price * 0.95);
-            case "OnlyVip":
-                {
-                    if (user.isVip()) {
-                        return (int) (price * 0.95);
-                    } else {
-                        return price;
-                    }
-                }
-            default:
-                throw new IllegalStateException("Should not be here!");
-        }
+    public static int calculatePrice(BiFunction<Integer, User, Integer> discountStrategy, int price, User user) {
+        return discountStrategy.apply(price, user);
     }
 }
