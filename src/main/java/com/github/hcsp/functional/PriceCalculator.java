2b5152b097db42fe2141f75ed12e75499e1e4191
朱骏杰 <zhujunjie@sundear.com>
2021-11-18 11:06:37 +0800

    finish

--- src/main/java/com/github/hcsp/functional/PriceCalculator.java
+++ src/main/java/com/github/hcsp/functional/PriceCalculator.java
@@ -18,28 +18,36 @@ public class PriceCalculator {
     // 使用函数式接口重构这个方法，将原先的三种策略作为参数传入
     //
     // 你可以选择自己声明一个接口，例如DiscountStrategy：
-    // static int calculatePrice(DiscountStrategy strategy, int price, User user)
+
+    interface DiscountStrategy {
+        int discount(int price, User user);
+    }
+
+    static int calculatePrice(DiscountStrategy strategy, int price, User user) {
+        return strategy.discount(price, user);
+    }
+
     //
     // 或是使用JDK自带的函数式接口BiFunction
     //
     // static int calculatePrice(BiFunction<Integer,User,Integer> strategy, int price, User user)
 
-    public static int calculatePrice(String discountStrategy, int price, User user) {
-        switch (discountStrategy) {
-            case "NoDiscount":
-                return price;
-            case "Discount95":
-                return (int) (price * 0.95);
-            case "OnlyVip":
-                {
-                    if (user.isVip()) {
-                        return (int) (price * 0.95);
-                    } else {
-                        return price;
-                    }
-                }
-            default:
-                throw new IllegalStateException("Should not be here!");
-        }
-    }
+//    public static int calculatePrice(String discountStrategy, int price, User user) {
+//        switch (discountStrategy) {
+//            case "NoDiscount":
+//                return price;
+//            case "Discount95":
+//                return (int) (price * 0.95);
+//            case "OnlyVip":
+//                {
+//                    if (user.isVip()) {
+//                        return (int) (price * 0.95);
+//                    } else {
+//                        return price;
+//                    }
+//                }
+//            default:
+//                throw new IllegalStateException("Should not be here!");
+//        }
+//    }
 }
