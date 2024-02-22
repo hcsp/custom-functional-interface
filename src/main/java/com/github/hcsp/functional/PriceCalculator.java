e66e9ef6fd0a600464e0d6a00af9c1f24d12f834
rf4github <39894049+rf818@users.noreply.github.com>
2024-02-22 22:38:49 +0800

    使用BiFunction (#142)
    使用BiFunction (#142)

--- src/main/java/com/github/hcsp/functional/PriceCalculator.java
+++ src/main/java/com/github/hcsp/functional/PriceCalculator.java
@@ -1,11 +1,13 @@
 package com.github.hcsp.functional;
 
+import java.util.function.BiFunction;
+
 public class PriceCalculator {
     public static void main(String[] args) {
         int originalPrice = 100;
         User vipUser = User.vip("张三");
         // 不打折
-        calculatePrice((price, user) -> price, originalPrice, vipUser);
+        System.out.printf(String.valueOf(calculatePrice((price, user) -> price, originalPrice, vipUser)));
         // 全场95折
         calculatePrice((price, user) -> (int) (price * 0.95), originalPrice, vipUser);
         // 只有VIP打95折，其他人保持原价
@@ -24,22 +26,7 @@ public class PriceCalculator {
     //
     // static int calculatePrice(BiFunction<Integer,User,Integer> strategy, int price, User user)
 
-    public static int calculatePrice(String discountStrategy, int price, User user) {
-        switch (discountStrategy) {
-            case "NoDiscount":
-                return price;
-            case "Discount95":
-                return (int) (price * 0.95);
-            case "OnlyVip":
-                {
-                    if (user.isVip()) {
-                        return (int) (price * 0.95);
-                    } else {
-                        return price;
-                    }
-                }
-            default:
-                throw new IllegalStateException("Should not be here!");
-        }
+    public static int calculatePrice(BiFunction<Integer, User, Integer> strategy, int price, User user) {
+        return strategy.apply(price, user);
     }
 }
